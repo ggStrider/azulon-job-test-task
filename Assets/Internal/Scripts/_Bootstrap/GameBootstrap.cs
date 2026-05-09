c56866a4723c8760c86e3d1143c7ba2dadd31d2b
Yaroslav Oslam <ggStrider1@gmail.com>
2026-05-09 16:30:22 +0300

    feat: register services in bootstrap

--- Assets/Internal/Scripts/_Bootstrap/GameBootstrap.cs
+++ Assets/Internal/Scripts/_Bootstrap/GameBootstrap.cs
@@ -1,5 +1,8 @@
 using Internal.Scripts._Bootstrap.Locator;
+
 using Internal.Scripts.Core.Data.Inventory;
+using Internal.Scripts.Features.Click;
+using Internal.Scripts.Features.Currency;
 using UnityEngine;
 
 namespace Internal.Scripts._Bootstrap
@@ -9,13 +12,13 @@ namespace Internal.Scripts._Bootstrap
     {
         private void Awake()
         {
-            var inventory = CreateInventory();
-            ServiceLocator.Register<IInventory>(to: inventory);
-        }
+            var currencyService = new CurrencyService();
+            var inventory = new PlayerInventory();
+            var clickService = new ClickService(currencyService, inventory);
 
-        private IInventory CreateInventory()
-        {
-            return new PlayerInventory();
+            ServiceLocator.Register<ICurrencyService>(currencyService);
+            ServiceLocator.Register<IInventory>(inventory);
+            ServiceLocator.Register<IClickService>(clickService);
         }
     }
 }
