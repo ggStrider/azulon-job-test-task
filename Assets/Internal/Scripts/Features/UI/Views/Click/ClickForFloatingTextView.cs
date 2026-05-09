870bcf8b5ab81c2ea48f54b5e322c0900e048bce
Yaroslav Oslam <ggStrider1@gmail.com>
2026-05-09 19:16:29 +0300

    refactor: use objectpool instead of instantiate&destroy

--- Assets/Internal/Scripts/Features/UI/Views/Click/ClickForFloatingTextView.cs
+++ Assets/Internal/Scripts/Features/UI/Views/Click/ClickForFloatingTextView.cs
@@ -1,36 +1,57 @@
 using Internal.Scripts._Bootstrap.Locator;
 using Internal.Scripts.Features.Click;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.UI;
 
 namespace Internal.Scripts.Features.UI.Views.Click
 {
-    public class ClickView : MonoBehaviour
+    public class ClickForFloatingTextView : MonoBehaviour
     {
         [SerializeField] private Button _clickButton;
         [SerializeField] private FloatingTextView _floatingTextPrefab;
         [SerializeField] private Transform _floatingTextSpawnPoint;
 
+        [Space]
+        [SerializeField] private int _defaultPoolSize = 5;
+        [SerializeField] private int _maxPoolSize = 20;
+
         private IClickService _clickService;
+        private ObjectPool<FloatingTextView> _floatingTextPool;
 
         private void Start()
         {
             _clickService = ServiceLocator.Get<IClickService>();
+            
+            _floatingTextPool = new ObjectPool<FloatingTextView>(
+                createFunc: () => Instantiate(_floatingTextPrefab, _floatingTextSpawnPoint),
+                actionOnGet: view => view.gameObject.SetActive(true),
+                actionOnRelease: view => view.gameObject.SetActive(false),
+                actionOnDestroy: view => Destroy(view.gameObject),
+                defaultCapacity: _defaultPoolSize,
+                maxSize: _maxPoolSize);
+
             _clickButton.onClick.AddListener(OnClickButton);
         }
 
         private void OnDestroy()
         {
             _clickButton.onClick.RemoveListener(OnClickButton);
+            _floatingTextPool.Dispose();
         }
 
         private void OnClickButton()
         {
             var clickValue = _clickService.ClickValue.Value;
-            _clickService.Click();
+            var floatingText = _floatingTextPool.Get();
             
-            var floatingText = Instantiate(_floatingTextPrefab, _floatingTextSpawnPoint);
-            floatingText.Initialize(clickValue);
+            floatingText.transform.position = _floatingTextSpawnPoint.position;
+            floatingText.Initialize(clickValue, OnFloatingTextComplete);
+        }
+
+        private void OnFloatingTextComplete(FloatingTextView floatingText)
+        {
+            _floatingTextPool.Release(floatingText);
         }
     }
 }
--- Assets/Internal/Scripts/Features/UI/Views/Click/FloatingTextView.cs
+++ Assets/Internal/Scripts/Features/UI/Views/Click/FloatingTextView.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using TMPro;
 using UnityEngine;
 
@@ -10,24 +12,31 @@ namespace Internal.Scripts.Features.UI.Views.Click
         [SerializeField] private float _moveSpeed = 100f;
 
         private float _timer;
+        private Action<FloatingTextView> _onComplete;
 
-        public void Initialize(int value)
+        private readonly StringBuilder _stringBuilder = new StringBuilder();
+
+        public void Initialize(int value, Action<FloatingTextView> onComplete)
         {
-            _text.text = $"+{value}";
+            _stringBuilder.Clear();
+            _stringBuilder.Append("+");
+            _stringBuilder.Append(value);
+            _text.text = _stringBuilder.ToString();
+
+            _timer = 0f;
+            _onComplete = onComplete;
+            _text.alpha = 1f;
         }
 
         private void Update()
         {
             _timer += Time.deltaTime;
 
-            var progress = _timer / _duration;
             transform.Translate(Vector3.up * (_moveSpeed * Time.deltaTime));
-            
-            var alpha = Mathf.Lerp(1f, 0f, progress);
-            _text.alpha = alpha;
+            _text.alpha = Mathf.Lerp(1f, 0f, _timer / _duration);
 
             if (_timer >= _duration)
-                Destroy(gameObject);
+                _onComplete?.Invoke(this);
         }
     }
 }
